d10a94989e8856642c8ba8294e026ee7944ef69c
Juan Jesus Padron Hernandez <jjph22@gmail.com>
2021-04-25 18:08:39 +0100

    some changes

--- src/Shared/infrastructure/persistence/postgre/PostgreRepository.ts
+++ src/Shared/infrastructure/persistence/postgre/PostgreRepository.ts
@@ -11,7 +11,8 @@ export class PostgreRepository implements Repository {
   private client?: any;
   // private readonly hostDB = process.env.DB || 'postgres://test-user@localhost/test-db';
   private readonly options = {
-    host: 'localhost',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: Number(process.env.POSTGRES_PORT) || 5432,
     user: process.env.POSTGRES_USER || 'postgres',
     password: process.env.POSTGRES_PASSWORD || 'postgres',
     max: 20,
